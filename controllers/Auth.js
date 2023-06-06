4f104ed2509e531674d42032dd7467a09dc72557
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-06-05 21:20:06 -0700

    bcrypt installed, auth controller

---
+++ controllers/Auth.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+const router = express.Router();
+
+
+
+module.exports = router;
+
