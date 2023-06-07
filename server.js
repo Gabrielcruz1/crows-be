f753e700cb5ece2718fb0eb56db48e394c2bcb94
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-06-06 17:39:01 -0700

    debugging creating user

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
 
 require('dotenv').config();
 
 const {PORT, MONGODB_URI} = process.env 
+
+//Controllers 
+const authController = require("./controllers/auth");
+
+//MIDDLEWARE
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json());
+app.use("/auth", authController);
+
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>')
